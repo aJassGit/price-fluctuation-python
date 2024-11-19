cf7ba362a3461334223a89845bdf5fab67cef1f4
aJassGit <alanjassan@hotmail.com>
2024-11-19 18:36:17 -0300

    Change link to streamlit

--- ejercicio.py
+++ ejercicio.py
@@ -4,7 +4,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 ## ATENCION: Debe colocar la direccion en la que ha sido publicada la aplicacion en la siguiente linea\
-# url = 'https://tp8-555555.streamlit.app/'
+# url = 'https://tp8-58777-tut9bi2u9wrbzbhbmkidbx.streamlit.app'
 
 def mostrar_informacion_alumno():
     with st.container(border=True):
